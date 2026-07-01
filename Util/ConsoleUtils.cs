8b231f56017455e040554efea5c076245d757488
Black_Moss <kytheminecraft@outlook.com>
2026-07-02 01:07:55 +0800

    simple LogToConsole

--- Util/ConsoleUtils.cs
+++ Util/ConsoleUtils.cs
@@ -4,13 +4,23 @@ public static class ConsoleUtils
 {
     public static readonly ConsoleScript ConsoleInstance = ConsoleScript.instance;
 
-    public static void LogToConsole(ConsoleScript instance, string commandString)
+    public static void LogToConsole(ConsoleScript instance, string text)
     {
-        ReflectionUtils.InvokeMethod(instance, "RunCommandString", commandString);
+        ReflectionUtils.InvokeMethod(instance, "LogToConsole", text);
+    }
+
+    public static void LogToConsole(string text)
+    {
+        LogToConsole(ConsoleInstance, text);
     }
 
     public static void RunCommand(ConsoleScript instance, string commandString)
     {
         ReflectionUtils.InvokeMethod(instance, "RunCommandString", commandString);
+    }
+
+    public static void RunCommand(string commandString)
+    {
+        RunCommand(ConsoleInstance, commandString);
     }
 }
